e1d48d91b2b83a0da08f463e21cb56e8fe9b5342
peper11111 <peper.pleban@gmail.com>
2016-06-23 18:54:59 +0200

    Merge. Scalenie małych kontrolerów w duże

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -1,12 +1,6 @@
 (function () {
     var app = angular.module('bookStore', ['ngStorage']);
 
-    app.controller('BookCategoryController', function ($scope, $http) {
-        $http.get('/api/book/category/all').success(function (result) {
-            $scope.categories = result;
-        });
-    });
-
     app.controller('SessionController', function () {
         this.loggedIn = localStorage.getItem('loggedIn');
 
@@ -19,34 +13,24 @@
         };
     });
 
-    app.controller('ProductsController', function ($scope,$http) {
-        this.books = null;
-        $http({method: 'GET',
-            url: 'api/book/all' ,
-            respondType: 'json'}).then(function (response) {
-            $scope.books = response.data;
-            
-        },function(response){
-            alert("Dupa");
-        });
-        // this.books = [{'nazwa': 'Harry Potter i Kiebasa Prymasa','autor':'J.K.Rollin\'','cena':9.20},
-        //     {'nazwa': 'Harry Potter i Podwawelska','autor':'J.K.Rollin\'','kategoria':'Komiedia'}];
-        
-        this.addToCart = function(){
-        }
-    });
-
-    app.controller('FeaturedBooksController', function ($scope, $http) {
+    app.controller('BooksController', function ($scope, $http) {
         $http.get('/api/book/all').success(function (result) {
             $scope.books = result;
         });
+
+        $http.get('/api/book/category/all').success(function (result) {
+            $scope.categories = result;
+        });
     });
 
 
-    app.controller('UserOrderChechController', function ($scope, $http) {
+    app.controller('UserOrderController', function ($scope, $http) {
         $http.get('/api/order').success(function (result) {
             $scope.orders = result;
         });
     });
-    
+
+    app.controller('CartController', function ($scope, $http) {
+        
+    })
 })();
