4b0e3f56594bd40edf0311a716aad4080d9e3484
kiker7 <rafalrutyna00@gmail.com>
2016-06-23 22:53:31 +0200

    refactor nazw obrazków ksiazek

--- src/test/users.sql
+++ src/test/users.sql
@@ -1,7 +1,17 @@
 INSERT INTO role(user_id , role) VALUES (null , 'USER_ROLE');
-INSERT INTO users(id , role_id , activated , email , password , token , username , acv_time_stamp)
-VALUES (1 , 1 , FALSE , 'test@test.pl' , 'testtest' , 'test' , 'test' , to_date('2014/04/25', 'YYYY/MM/DD'));
-UPDATE role SET user_id = 1 WHERE id = 1;
+INSERT INTO role(user_id , role) VALUES (null , 'ADMIN_ROLE');
 
-INSERT INTO book_rating(comment , nick , rating_date , rating , book_id) values ('TESTC' , 'TESTN' , current_date , 1 , 1);
-INSERT INTO customer_message(customer_email , date , message) values ('test@test.pl' , current_date , 'testm');
+INSERT INTO users(id , role_id , activated , email , password , token , username , acv_time_stamp)
+VALUES (1 , 1 , FALSE , 'admin@test.pl' , 'admin' , 'admin' , 'admin' , to_date('2014/04/25', 'YYYY/MM/DD'));
+INSERT INTO users(id , role_id , activated , email , password , token , username , acv_time_stamp)
+VALUES (1 , 1 , FALSE , 'user@test.pl' , 'user' , 'user' , 'user' , to_date('2014/04/25', 'YYYY/MM/DD'));
+
+UPDATE role SET user_id = 1 WHERE id = 1;
+UPDATE role SET user_id = 2 WHERE id = 2;
+
+INSERT INTO book_rating(comment , nick , rating_date , rating , book_id) values ('Bardzo dobra książka' , 'Mól książkowy' , current_date , 5 , 1);
+INSERT INTO book_rating(comment , nick , rating_date , rating , book_id) values ('Trochę nudna' , 'anonim' , current_date , 2 , 2);
+INSERT INTO book_rating(comment , nick , rating_date , rating , book_id) values ('Końcówka do przewidzenia...' , 'Andrzej' , current_date , 3 , 10);
+
+
+INSERT INTO customer_message(customer_email , date , message) values ('user@test.pl' , current_date , 'Zamówienie gotowe do odbioru');
